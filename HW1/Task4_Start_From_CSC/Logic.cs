bdd95743b41416e03e0618d03756e96c3eaca268
Balgabekov Ilyas <nefosfatus@gmail.com>
2019-12-24 23:37:06 +0600

    Task4 Complete!

---
+++ HW1/Task4_Start_From_CSC/Logic.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp
+{
+    public class Logic
+    {
+        public string Method()
+        {
+            return "I am string in Logic.";
+        }
+    }
+}
---
+++ HW1/Task4_Start_From_CSC/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic logic = new Logic();
+            Console.WriteLine(logic.Method());
+        }
+    }
+}
