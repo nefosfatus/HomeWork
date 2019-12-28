4b78aa7391356dbdbf01dc376b1b6b486660cfc5
Balgabekov Ilyas <nefosfatus@gmail.com>
2019-12-28 17:23:35 +0600

    Добавил комментарии к заданию 1

--- HW2/Task1_NewtonMethod/Task1_NewtonMethod/Task1_NewtonMethod/Program.cs
+++ HW2/Task1_NewtonMethod/Task1_NewtonMethod/Task1_NewtonMethod/Program.cs
@@ -8,20 +8,30 @@ using System.Threading.Tasks;
 
 namespace Task1_NewtonMethod
 {
+    /// <summary>
+    /// Main class there will be called other programm components
+    /// </summary>
     class Program
     {
+
         static void Main(string[] args)
         {
-
+            //Create instances
             MainCalculator calculator = new MainCalculator();
             Input userInput = new Input();
 
+            //Initialize user input
             userInput.GetUserInput();
 
+            //Get user input
             double userValue = userInput.Value;
             double userRoot = userInput.Root;
+
+            //Calculate by Newton method 
             double myValue = calculator.NewtonMethod(userValue, userRoot);
+            //Calculate by Math library method
             double mathValue = calculator.StandartMethod(userValue, userRoot);
+            //Compare mathods output
             calculator.CompareMethods(myValue, mathValue);
 
             Console.ReadKey();
@@ -31,12 +41,15 @@ namespace Task1_NewtonMethod
 
     }
     /// <summary>
-    /// Класс для работы с входными данными
+    /// Class for work with user
     /// </summary>
     class Input
     {
         public double Value { get; set; }
         public double Root { get; set; }
+        /// <summary>
+        /// Method reuests user input and saves it.
+        /// </summary>
         public void GetUserInput()
         {
             try
@@ -54,41 +67,76 @@ namespace Task1_NewtonMethod
         
     }
 
+    /// <summary>
+    /// Class for calculations
+    /// </summary>
     class MainCalculator
     {
+        //Start parameters
         private double x0 = 1;
         const double approximation = 0.00000001;
-
+        /// <summary>
+        /// For calculation by the Newton Method
+        /// </summary>
+        /// <param name="val">Value</param>
+        /// <param name="root">Roor</param>
+        /// <returns>Calculated Value</returns>
         public double NewtonMethod(double val, double root)
         {
-            double xk = x0;
+            double previosXn = x0;
             var delta = x0;
-            var counter = 0;
 
-            while (approximation<delta)
+            try
             {
-                double funcX = ((Math.Pow(xk, root)) - val);
-                double derivativeX = (root * (Math.Pow(xk,root-1)));
-                double Xn = xk - (funcX / derivativeX);
-                delta = Math.Abs(Xn - xk);
-                xk = Xn;
-                Console.WriteLine(Xn);
-                counter++;
+                while (approximation<delta)
+                {
+                    double functionX = ((Math.Pow(previosXn, root)) - val); // f(Xn-1) funcion
+                    double derivativeFuncionX = (root * (Math.Pow(previosXn,root-1))); //f'(Xn-1) funciton
+                    double Xn = previosXn - (functionX / derivativeFuncionX); // Xn = Xn-1 - f(Xn-1)/f'(Xn-1)
+                    delta = Math.Abs(Xn - previosXn); // delta between last two calculation
+                    previosXn = Xn;
+                }
+                return previosXn;
             }
-            return xk;
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
         }
+
+        /// <summary>
+        /// For calculation by the Math library method
+        /// </summary>
+        /// <param name="val">user Value</param>
+        /// <param name="root">user Root</param>
+        /// <returns>Calculated Value</returns>
         public double StandartMethod(double val, double root)
         {
+            try
+            {
             double Xn = Math.Pow(val, (1 / root));
             return Xn;
-        }
+            }
+            catch (Exception ex)
+            {
 
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+        /// <summary>
+        /// For compare two method results
+        /// </summary>
+        /// <param name="myMethodResult">Newton's method result</param>
+        /// <param name="otherMerthodResult">Math method result</param>
         public void CompareMethods(double myMethodResult, double otherMerthodResult)
         {
             Console.WriteLine("Вычеслено методом Ньютона: " + myMethodResult);
             Console.WriteLine("Вычеслено стандартной библиотекой: " + otherMerthodResult);
-            var differenceBetweenMethod = Math.Abs(myMethodResult - otherMerthodResult);
-            Console.WriteLine("Разница между вычисленными значениями: " + differenceBetweenMethod);
+            var differenceBetweenMethods = Math.Abs(myMethodResult - otherMerthodResult);
+            Console.WriteLine("Разница между вычисленными значениями: " + differenceBetweenMethods);
         }
         
     }     
